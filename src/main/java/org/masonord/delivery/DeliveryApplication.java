c7151b557c9e4f160e81a391f7289d730718d88a
Masonord <trotf233@gmail.com>
2023-10-19 10:41:02 +0200

    set up hibernate with db

--- src/main/java/org/masonord/delivery/DeliveryApplication.java
+++ src/main/java/org/masonord/delivery/DeliveryApplication.java
@@ -2,6 +2,9 @@ package org.masonord.delivery;
 
 import java.util.Arrays;
 
+import org.hibernate.Session;
+import org.masonord.delivery.model.EmployeeEntity;
+import org.masonord.delivery.util.HibernateUtil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,24 +13,28 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeliveryApplication {
+	private static final Session session = HibernateUtil.getSessionFactory().openSession();
+
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//
+//
+//		return args -> {
+//
+//		};
+//	}
+
+
 
 	public static void main(String[] args) {
+		EmployeeEntity emp = new EmployeeEntity();
+		emp.setEmail("demo-user@mail.com");
+		emp.setFirstName("demo");
+		emp.setLastName("user");
+		session.beginTransaction().commit();
+		session.save(emp);
+		session.close();
 		SpringApplication.run(DeliveryApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
 	}
 
 }
---
+++ src/main/java/org/masonord/delivery/util/HibernateUtil.java
@@ -0,0 +1,37 @@
+package org.masonord.delivery.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            if (sessionFactory == null) {
+                StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder()
+                        .configure("hibernate.cfg.xml").build();
+
+                Metadata metadata = new MetadataSources(standardServiceRegistry)
+                        .getMetadataBuilder()
+                        .build();
+
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            }
+            return sessionFactory;
+        }catch(Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
