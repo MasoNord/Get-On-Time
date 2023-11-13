c39c548c8ab6cad1d0d6912138277a55da8f2251
MasoNord <trotf233@gmail.com>
2023-11-14 00:50:33 +0300

    add schema class for a singnup request

---
+++ src/main/java/org/masonord/delivery/controller/v1/request/UserSignupRequest.java
@@ -0,0 +1,35 @@
+package org.masonord.delivery.controller.v1.request;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Setter
+@Getter
+@Accessors(chain = true)
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserSignupRequest {
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String role;
+
+    private String workingHours;
+
+    private String courierType;
+}
