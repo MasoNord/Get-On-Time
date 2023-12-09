09e2a4e9214b87dfd62107a73446dca199cc7173
MasoNord <trotf233@gmail.com>
2023-12-09 23:55:30 +0300

    add values to enums

--- src/main/java/org/masonord/delivery/enums/OrderStatusType.java
+++ src/main/java/org/masonord/delivery/enums/OrderStatusType.java
@@ -1,4 +1,16 @@
 package org.masonord.delivery.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum OrderStatusType {
+
+    PENDING("pending"),
+    COOKING("cooking"),
+    COOKED("cooked"),
+    DRIVE("drive");
+
+    private final String value;
 }
