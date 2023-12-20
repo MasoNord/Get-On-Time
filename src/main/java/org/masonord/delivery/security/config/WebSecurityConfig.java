00dcf6ba3e4f5e2bb7ad0f0526a6e266b49eb26a
MasoNord <trotf233@gmail.com>
2023-12-21 00:45:09 +0300

    refactoring: make some minor changes to get things working properly

--- src/main/java/org/masonord/delivery/security/config/WebSecurityConfig.java
+++ src/main/java/org/masonord/delivery/security/config/WebSecurityConfig.java
@@ -1,17 +1,28 @@
-package org.masonord.delivery.config;
+package org.masonord.delivery.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.masonord.delivery.config.ContextConfig;
 import org.masonord.delivery.enums.UserRoles;
+import org.masonord.delivery.security.filter.CustomAuthenticationFilter;
+import org.masonord.delivery.security.filter.CustomAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
@@ -25,26 +36,26 @@ public class WebSecurityConfig {
             "/",
             "/home",
             "/logout",
-            "/login",
-//            "/api/**"
+            "/api/v1/user/signup",
+            "/api/v1/auth/login/**",
+            "/api/v1/user/token/refresh/**",
     };
 
-    private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
-
+    private final UserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
-//                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.configure(http))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
-                                .requestMatchers("/api/v1/order/**").hasAnyRole(UserRoles.COURIER.getValue(), UserRoles.ADMIN.getValue(), UserRoles.CUSTOMER.getValue(), UserRoles.RESTAURANT_WORKER.getValue())
-                                .requestMatchers("/api/v1/courier/**").hasAnyRole(UserRoles.COURIER.getValue(), UserRoles.ADMIN.getValue())
-                                .requestMatchers("/api/v1/customer/**").hasAnyRole(UserRoles.CUSTOMER.getValue(), UserRoles.ADMIN.getValue())
-                                .requestMatchers("/api/v1/user/**").hasRole(UserRoles.ADMIN.getValue())
-                                .requestMatchers("/api/v1/completedOrders/**").hasAnyRole(UserRoles.COURIER.getValue(), UserRoles.ADMIN.getValue())
+                                .requestMatchers("/api/v1/order/**").hasAnyAuthority(UserRoles.COURIER.getValue(), UserRoles.ADMIN.getValue(), UserRoles.CUSTOMER.getValue(), UserRoles.RESTAURANT_WORKER.getValue())
+                                .requestMatchers("/api/v1/courier/**").hasAnyAuthority(UserRoles.COURIER.getValue(), UserRoles.ADMIN.getValue())
+                                .requestMatchers("/api/v1/customer/**").hasAnyAuthority(UserRoles.CUSTOMER.getValue(), UserRoles.ADMIN.getValue())
+                                .requestMatchers("/api/v1/user/**").hasAuthority(UserRoles.ADMIN.getValue())
+                                .requestMatchers("/api/v1/completedOrders/**").hasAnyAuthority(UserRoles.COURIER.getValue(), UserRoles.ADMIN.getValue())
                                 .anyRequest()
                                 .authenticated()
                 )
@@ -54,76 +65,25 @@ public class WebSecurityConfig {
                                 .maxSessionsPreventsLogin(true)
 
                 )
-                .authenticationProvider(authenticationProvider)
+                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean(userDetailsService, ContextConfig.bCryptPasswordEncoder())))
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .logout(logout ->
                         logout.logoutUrl("api/v1/auth/logout")
                                 .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
 
                 );
+
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManagerBean(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) throws Exception {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
+        return new ProviderManager(authenticationProvider);
+    }
 
-
-
-//    @Order(1)
-//    @Configuration
-//    public static class HttpSecurityConfiguration {
-//        @Bean
-//        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//            http
-//                    .authorizeHttpRequests((request) ->
-//                        request
-//                                .requestMatchers("/api/**", "/api/v1/user/signup").permitAll()
-//                                .anyRequest().authenticated()
-//                    )
-//                    .exceptionHandling((exception) ->
-//                        exception
-//                                .authenticationEntryPoint((res, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-//                    )
-//                    .sessionManagement((session) ->
-//                        session
-//                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-//                                .invalidSessionUrl("/invalidSession.htm")
-//                                .maximumSessions(1)
-//                                .maxSessionsPreventsLogin(true)
-//                    );
-//
-//            return http.build();
-//        }
-//
-//        @Bean
-//        public PasswordEncoder passwordEncoder() {
-//            return new BCryptPasswordEncoder();
-//        }
-//
-//        @Bean
-//        CustomUserDetailsService customUserDetailsService() {
-//            return new CustomUserDetailsService();
-//        }
-//    }
-//
-//    @Order(2)
-//    @Configuration
-//    public static class LoginWebSecurityConfiguration {
-//
-//        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//            http
-//                    .
-//        }
-//    }
-//
 }
